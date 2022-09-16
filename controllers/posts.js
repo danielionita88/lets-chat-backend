13fa8bfffe12b1acda913c6476d5f90af262351c
Daniel Ionita <danielionita88@yahoo.com>
2022-09-16 10:34:49 -0500

    added routes and functionality for updating/deleting a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,10 +3,10 @@ const User = require("../models/User");
 
 exports.getPosts = async (req, res) => {
   try {
-    const posts = await Post.find()
-    res.status(200).json(posts)
-  }catch(e){
-    res.status(500).json(e)
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (e) {
+    res.status(500).json(e);
   }
 };
 
@@ -18,6 +18,24 @@ exports.createPost = async (req, res) => {
     const post = await newPost.save();
     await user.updateOne({ $push: { posts: post._id } });
     res.status(201).json(post);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+exports.updatePost = async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, { $set: req.body });
+    res.status(200).json("Post updated succesfully!");
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json("Post deleted succesfully!");
   } catch (e) {
     res.status(500).json(e);
   }
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const postsController = require('../controllers/posts')
+const postsController = require("../controllers/posts");
 
 router.get("/", postsController.getPosts);
 
-router.post('/', postsController.createPost)
+router.post("/", postsController.createPost);
+
+router.put("/:id", postsController.updatePost);
+
+router.delete("/:id", postsController.deletePost);
 
 module.exports = router;
- 
